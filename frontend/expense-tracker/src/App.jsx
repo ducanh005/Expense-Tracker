03d93458ac0c5095b9c97504975a496cfd0c8d54
ducanh005 <vuducanh039@gmail.com>
2025-09-15 20:59:02 +0700

    Installing Required Dependencies and definding App Routes

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,8 +1,40 @@
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
+
+
 const App = ()=>{
   return (
-    <div className="text-3xl text-purple-200">Expense Tracker</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login/>} />
+          <Route path="/signUp" exact element={<Signup/>} />
+          <Route path="/dashboard" exact element={<Home/>} />
+          <Route path="/income" exact element={<Income/>} />
+          <Route path="/expense" exact element={<Expense/>} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
 export default App;
+
+const Root = () => {
+  // Check if token exists in localStorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  // Redirect to /dashboard if authenticated, otherwise to /login
+  return isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />;
+}
