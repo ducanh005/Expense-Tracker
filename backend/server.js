301a9fc8be6c6480eeb1c0790911943e109d3e7e
ducanh005 <vuducanh039@gmail.com>
2025-09-19 20:41:01 +0700

    connecting to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.use(cors(
 ));
 
 app.use(express.json());
+
+connectDB()
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
