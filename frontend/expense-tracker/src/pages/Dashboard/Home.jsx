34e303d7e6a21ef7cf8e362e70eaada67c809931
ducanh005 <vuducanh039@gmail.com>
2025-09-29 22:18:02 +0700

    Expense Details Section

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -10,6 +10,7 @@ import {LuHandCoins, LuWalletMinimal} from "react-icons/lu"
 import {IoMdCard} from "react-icons/io"
 import { addThousandsSeparator } from "../../utils/helper";
 import RecentTransaction from "../../components/Dashboard/RecentTransaction";
+import FinanceOverview from "../../components/Dashboard/FinanceOverview";
 const Home = ()=>{
   useUserAuth()
   const navigate = useNavigate()
@@ -24,7 +25,6 @@ const Home = ()=>{
         `${API_PATHS.DASHBOARD.GET_DATA}`
       )
       if(response.data){
-        console.log(response.data)
         setDashboardData(response.data)
       }
     }catch(error){
@@ -41,7 +41,7 @@ const Home = ()=>{
   return (
     <DashBoardLayout activeMenu= "Dashboard" >
       <div className="my-5 mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           <InfoCard
             icon={<IoMdCard/>}  
             label="Total Balance"
@@ -61,12 +61,17 @@ const Home = ()=>{
             color="bg-red-500"
           />
 
-        </div>
+        </div> */}
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
           <RecentTransaction
             transaction= {dashboardData?.recentTransactions}
             onSeeMore={()=> navigate("/expense")}
+          />
+          <FinanceOverview
+            totalBalance = {dashboardData?.totalBalance || 0}
+            totalIncome = {dashboardData?.totalIncome || 0}
+            totalExpense = {dashboardData?.totalExpense || 0}
           />
         </div>
       </div>
